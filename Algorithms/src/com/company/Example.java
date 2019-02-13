ca8b9029e477e3438ff89a5ab5ef3e51a15a5c96
Anderkaisa <lcs199519lcs@163.com>
2019-02-13 09:55:59 +0800

    none

--- Algorithms/src/com/company/Example.java
+++ Algorithms/src/com/company/Example.java
@@ -1,23 +1,18 @@
 package com.company;
 
-public abstract class Example {
-    private static void sort(Comparable[] a){}
-    private static  boolean less(Comparable v,Comparable w){
-        return v.compareTo(w)<0;
-    }
-    private static void show(Comparable[] a){
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+" ");
-            System.out.println();
-        }
-    }
-    private static boolean isSorted(Comparable[] a){
-        for (int i = 0; i <a.length ; i++) {
-            if (less(a[i],a[i-1])){
-                return false;
-            }
-        }
-        return true;
-    }
-    
+/**
+ * @author Anderkaisa
+ */
+public interface Example {
+    public void sort(Comparable[] a);
+
+    public boolean less(Comparable v, Comparable w);
+
+    public void show(Comparable[] a);
+
+    public boolean isSorted(Comparable[] a);
+
+    public void exch(Comparable[] a, int i, int j);
+
 }
+
--- Algorithms/src/com/company/Selection.java
+++ Algorithms/src/com/company/Selection.java
@@ -1,14 +1,58 @@
 package com.company;
 
-public class Selection extends Example {
+/**
+ * @author Anderkaisa
+ */
+public class Selection implements Example {
 
-    public static void sort(Comparable[] a){
-        int N=a.length;
-        for (int i=0;i<N;i++){
-            int min=i;
-            for (int j = i+1; j < N; j++) {
+    public static void main(String[] args) {
+        String[] a=In.readStrings();
+        Selection test=new Selection();
+        test.sort(a);
+        assert test.isSorted(a);
+        test.show(a);
+    }
 
+    @Override
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    exch(a, i, min);
+                }
             }
         }
+    }
+
+    @Override
+    public boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    @Override
+    public void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+            StdOut.println();
+        }
+    }
+
+    @Override
+    public boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 }
