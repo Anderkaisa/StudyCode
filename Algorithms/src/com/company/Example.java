7b5bee54f66e9fc28c372d04c8ade807ac8fee7c
Anderkaisa <lcs199519lcs@163.com>
2019-02-13 08:43:57 +0800

    anderkaisa
    commit

---
+++ Algorithms/src/com/company/Example.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public abstract class Example {
+    private static void sort(Comparable[] a){}
+    private static  boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    private static void show(Comparable[] a){
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+            System.out.println();
+        }
+    }
+    private static boolean isSorted(Comparable[] a){
+        for (int i = 0; i <a.length ; i++) {
+            if (less(a[i],a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
---
+++ Algorithms/src/com/company/Selection.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Selection extends Example {
+
+    public static void sort(Comparable[] a){
+        int N=a.length;
+        for (int i=0;i<N;i++){
+            int min=i;
+            for (int j = i+1; j < N; j++) {
+
+            }
+        }
+    }
+}
